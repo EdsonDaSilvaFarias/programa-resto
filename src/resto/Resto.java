f351bc352e53f070c1079e7bc8e3c4983bd3860d
ed <ed@ed-STI>
2017-11-25 20:08:47 -0300

    commit primeiro repositorio

--- src/resto/Resto.java
+++ src/resto/Resto.java
@@ -19,7 +19,7 @@ public class Resto {
         if ( (x % 2)== 0){
            System.out.println("par"); 
         }else {
-            System.out.println("impa ");
+            System.out.println("impa");
         }
         
         
